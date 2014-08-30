1958a6640b01c456312b2b1dbce8b34ffc267581
Sri Prasanna <k.sriprasanna@gmail.com>
2014-08-30 11:19:25 -0500

    cards also needs a default user

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe Card, :type => :model do
   describe "Validations" do
     it { should validate_presence_of(:uuid) }
     it { should validate_presence_of(:type) }
+    it { should validate_presence_of(:user_id) }
   end
 end
