fb1e9da154e960842b57433077aa8cc2e53401dc
Sri Prasanna <k.sriprasanna@gmail.com>
2014-08-30 09:30:43 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TechRadarGame::Application.config.secret_key_base = '06da739955cb3d438979177d539bc591a62ed0e4da31617453fa08864b3d4cd016aeadb96ee7563c2475e8e61f28d909169ed02b5ac53934fe9daea11c0e7228'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TechRadarGame::Application.config.session_store :cookie_store, key: '_TechRadarGame_session'
